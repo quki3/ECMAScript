506bc6a222ed424e138bc4c1fdebcc0c1fe91f6f
gaBriel nesTor bruFau <70489784+qukiiignb@users.noreply.github.com>
2023-07-01 21:00:52 -0300

    Create build-in-object-2.js

---
+++ UNLOCK-CHALLENGES/1/a/build-in-object-2.js
@@ -0,0 +1,19 @@
+const o = {
+  a: 1,
+  b: 2,
+  // __proto__ sets the [[Prototype]]. It's specified here
+  // as another object literal.
+  __proto__: {
+    b: 3,
+    c: 4,
+  },
+};
+
+// o.[[Prototype]] has properties b and c.
+// o.[[Prototype]].[[Prototype]] is Object.prototype (we will explain
+// what that means later).
+// Finally, o.[[Prototype]].[[Prototype]].[[Prototype]] is null.
+// This is the end of the prototype chain, as null,
+// by definition, has no [[Prototype]].
+// Thus, the full prototype chain looks like:
+// { a: 1, b: 2 } ---> { b: 3, c: 4 } ---> Object.prototype ---> null
