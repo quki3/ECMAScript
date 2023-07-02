de434ab9f9313582c1f2106ac5c999f53ffa0991
gaBriel nesTor bruFau <70489784+qukiiignb@users.noreply.github.com>
2023-07-02 15:34:17 -0300

    Update unit-test.js

--- React/UPLOCK-CHALLENGES/1/c/unit-test.js
+++ React/UPLOCK-CHALLENGES/1/c/unit-test.js
@@ -9,6 +9,7 @@ test('renders front button', () => {
 });
 
 test('clicking front button triggers the onClick event', () => {
+  // jest.fn() - is a function provided by the Jest testing framework. It is used to create a mock function, which allows you to simulate and control the behavior of a function during testing.
   const onClickMock = jest.fn();
   render(<FrontButton onClick={onClickMock} />);
   const frontButton = screen.getByText('Front');
