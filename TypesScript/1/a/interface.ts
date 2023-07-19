66a2f2109d086739d40132cecbc8f717ab3fe48e
Quki <70489784+quki3@users.noreply.github.com>
2023-07-19 15:31:11 -0300

    Create interface.ts

---
+++ TypesScript/1/a/interface.ts
@@ -0,0 +1,10 @@
+// describe a object that you will use after
+interface User {
+  name: String; // NOTE: use <;> instead of <,>
+  id: number;
+}
+// now you can use the new interface by using sintax like <:typeName after a variable declaration
+const user:User {
+  name:"exem",
+  id:0
+};
